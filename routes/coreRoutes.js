76f5ab1b1af8f2704100deb612780f0b2ced7e6a
Adityakr49 <adityakumarpilot@gmail.com>
2023-10-28 16:04:33 +0530

    third

---
+++ routes/coreRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const router = express.Router()
+const { register, login } = require('../controllers/core')
+router.post('/register', register)
+router.post('/login', login)
